8dbf3e4597967934a8a43c428e347d901787c2ed
Echeka <checafelfoldy@gmail.com>
2025-01-16 18:21:38 +0100

    Added functions to start solving the puzzle

--- 2023/puzzle_2/main.c
+++ 2023/puzzle_2/main.c
@@ -11,23 +11,21 @@ IDs of those games?
 #define RED_CUBES 12
 #define GREEN_CUBES 13
 #define BLUE_CUBES 14
+#define FALSE 0
+#define TRUE 1
 
 FILE *pfile;
+int sum; //the sum of the IDs of possible games
 
+void parse_text();
+int sum_if_valid(int isValid, int id);
+int evaluate_substring(char *sub);
+void skip_to_eol();
 
-
-//TODO look for string splitting lib/method
 
 int main()
 {
-    
-    int sum = 0;    //the sum of the IDs of possible games
-    int num;    //temportal number to store the possible game's IDs
-    int qred, qgreen, qblue;  //the quantities of dice extracted each time
-    qred = qgreen = qblue = 0;
-    int nice_games[100]; //stores the numbers of the games that are possible
-    
-    
+    sum = 0;
     
     pfile = fopen("input", "r+");
     
@@ -39,63 +37,101 @@ int main()
         
     }
     
+    parse_text();
+
+    return 0;
+}
+
+/**
+ * Parses and stores the text until it finds a punctuation symbol. Then, it redirects to
+ * other functions to store an id or check if the dice extraction is valid
+ **/
+ 
+void parse_text() {
+    
+    char c; //auxiliary character
+    char substring [15]; //stores parts of the text to analyze for data
+    int qred = 0, qgreen = 0, qblue = 0; //the quantities of dice extracted each time
+    int id;    //temportal number to store the game's IDs
+    int isValid = TRUE; //bool storing if the line is valid. Default to TRUE
+    
+    int count = 0;
+    
     while (!feof(pfile)) {
-        
-        printf("%d", BLUE_CUBES);
-        
-    }
-    
-
-    return 0;
-}
-
-
-//returns the number of the game if it is possible. Otherwise, returns 0 (false)
-int is_possible() {
-    
-    
-    
-    return 0;
-    
-}
-
-//Searches until it finds a new line escape secuence and returns an array of ints with info
-int* analyze_row() {
-    
-    char c;
-    
-    while (!feof(pfile)){
         
         c = fgetc(pfile);
         
+        if (c == '\n') {
+            
+            sum_if_valid(isValid, id);
+            //reset the variables
+            substring[0] = '\0';
+            id = 0;
+            isValid = TRUE;
+            
+        }
         
-        
-    }    
-}
-
-
-//gets the game number
-int get_game_number() {
-    
-    char c, cnumber;
-    int ctr = 0;
-    char buffer[25];
-    
-    while (c != ':' && !feof(pfile)) {
-        
-        c = fgetc(ptr);
-        buffer[i] = c;
-        ctr++;
-        
-    }
-    
-    for (int i = 0; i < strlen(buffer); i++) {
-        
-        //TODO define how to get game number
+        if (c == ',' || c == ';') {
+            
+            isValid = evaluate_substring(substring);
+            
+            if (isValid == FALSE) {
+                
+                skip_to_eol();
+                printf("%d\n", ++count);
+                
+            }
+            
+        }
         
     }
     
+}
+
+/**
+ * Sums the value of the id to the total if the parameter given is TRUE.
+ * Returns the value added.
+ **/
+
+int sum_if_valid(int validates, int id) {
     
-    return -1;
+    if (validates) sum += id;
+    
+    return id;
     
 }
+
+/**
+ * Returns TRUE if extraction is valid and FALSE otherwise. Also stores id number when found
+ **/
+
+int evaluate_substring(char *sub) {
+    
+    
+    
+    return FALSE;
+    
+}
+
+/**
+ * Moves the file pointer to the start of the next line
+ **/
+ 
+void skip_to_eol() {
+    
+    char c;
+    
+    while (!feof(pfile)) {
+        
+        c = fgetc(pfile);
+        
+        if (c == '\n') { //stop when found a new line
+            
+            fseek(pfile, -1, SEEK_CUR);
+            break;
+            
+        } 
+    }
+}
+
+
